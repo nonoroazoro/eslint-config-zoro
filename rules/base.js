83666b52028a6c81bee6bc0b994c31114b8d4dd7
nonoroazoro <xiaochao.k@gmail.com>
2016-05-05 15:25:05 +0800

    update

--- rules/base.js
+++ rules/base.js
@@ -7,7 +7,6 @@
     "rules": {
         "brace-style": [2, "allman"],
         "indent": [2, 4],
-        "no-console": 1,
         "no-unused-vars": [2, { "args": "none" }],
         "quotes": [2, "double"],
         "semi": [2, "always"]
