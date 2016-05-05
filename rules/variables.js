953c47746ca3cee232927a5b20269725ddb5ed17
nonoroazoro <xiaochao.k@gmail.com>
2016-05-05 16:46:18 +0800

    add variables

---
+++ rules/variables.js
@@ -0,0 +1,16 @@
+﻿module.exports = {
+    "rules": {
+        "init-declarations": 0,
+        "no-catch-shadow": 0,
+        "no-delete-var": 2,
+        "no-label-var": 0,
+        "no-restricted-globals": 0,
+        "no-shadow": 2,
+        "no-shadow-restricted-names": 2,
+        "no-undef": 2,
+        "no-undef-init": 0,
+        "no-undefined": 0,
+        "no-unused-vars": [2, { "vars": "local", "args": "none" }],
+        "no-use-before-define": 2
+    }
+};
