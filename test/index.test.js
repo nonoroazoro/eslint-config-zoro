797fe4057977eb2a8bc8a0e2f56f81cce675b175
nonoroazoro <xiaochao.k@gmail.com>
2016-05-06 10:38:02 +0800

    add test

---
+++ test/index.test.js
@@ -0,0 +1,21 @@
+﻿const fs = require("fs");
+const should = require("should");
+const index = require("../");
+
+describe("index", function ()
+{
+    it("should load all rules.", function (done)
+    {
+        Object.keys(index).forEach((key) =>
+        {
+            should.exist(index[key]);
+        });
+
+        index.extends.forEach((p_filePath) =>
+        {
+            fs.statSync(p_filePath);
+        });
+
+        done();
+    });
+});
