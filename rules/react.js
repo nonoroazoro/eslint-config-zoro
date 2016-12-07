05754bb56e3c8ff2a4b3404bd5f0e35cd0fd22e4
nonoroazoro <xiaochao.k@gmail.com>
2016-12-07 16:53:37 +0800

    update to eslint-plugin-react v6.8.0

--- rules/react.js
+++ rules/react.js
@@ -5,9 +5,11 @@
     },
     "rules": {
         "jsx-quotes": ["error", "prefer-double"],
+
         "react/display-name": "off",
         "react/forbid-component-props": "off",
         "react/forbid-prop-types": ["error", { forbid: ["any", "array", "object"] }],
+        "react/no-array-index-key": "error",
         "react/no-children-prop": "error",
         "react/no-danger": "off",
         "react/no-danger-with-children": "error",
@@ -17,15 +19,9 @@
         "react/no-direct-mutation-state": "error",
         "react/no-find-dom-node": "error",
         "react/no-is-mounted": "error",
-
-        // suspended。
-        "react/no-multi-comp": ["off", { ignoreStateless: true }],
-
+        "react/no-multi-comp": ["error", { ignoreStateless: true }],
         "react/no-render-return-value": "error",
-
-        // suspended。
         "react/no-set-state": "off",
-
         "react/no-string-refs": "error",
         "react/no-unescaped-entities": "error",
         "react/no-unknown-property": "error",
@@ -37,6 +33,7 @@
         "react/prefer-stateless-function": "error",
         "react/prop-types": ["error", { "ignore": ["children"], "customValidators": [] }],
         "react/react-in-jsx-scope": "error",
+        "react/require-default-props": "error",
         "react/require-optimization": "off",
         "react/require-render-return": "error",
         "react/self-closing-comp": "error",
@@ -52,6 +49,8 @@
             ]
         }],
         "react/sort-prop-types": "off",
+        "react/style-prop-object": "error",
+
         "react/jsx-boolean-value": ["error", "never"],
         "react/jsx-closing-bracket-location": ["error", "line-aligned"],
         "react/jsx-curly-spacing": ["error", "never", { "allowMultiline": true }],
@@ -76,6 +75,11 @@
         "react/jsx-pascal-case": "error",
         "react/jsx-sort-props": "off",
         "react/jsx-space-before-closing": ["error", "always"],
+        "react/jsx-tag-spacing": ["error", {
+            closingSlash: "never",
+            beforeSelfClosing: "always",
+            afterOpening: "never"
+        }],
         "react/jsx-uses-react": "error",
         "react/jsx-uses-vars": "error",
         "react/jsx-wrap-multilines": ["error", {
