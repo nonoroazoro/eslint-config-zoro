20cf6874824807db5bab06e6c942955ca433cfd1
nonoroazoro <xiaochao.k@gmail.com>
2016-12-07 17:05:45 +0800

    update to eslint v3.11.1

--- rules/errors.js
+++ rules/errors.js
@@ -4,7 +4,6 @@
  */
 module.exports = {
     "rules": {
-        "comma-dangle": "error",
         "no-cond-assign": ["error", "always"],
         "no-console": "warn",
         "no-constant-condition": "warn",
@@ -13,8 +12,8 @@ module.exports = {
         "no-dupe-args": "error",
         "no-dupe-keys": "error",
         "no-duplicate-case": "error",
-        "no-empty": ["error", { "allowEmptyCatch": true }],
         "no-empty-character-class": "error",
+        "no-empty": ["error", { "allowEmptyCatch": true }],
         "no-ex-assign": "error",
         "no-extra-boolean-cast": "off",
         "no-extra-parens": ["error", "functions"],
@@ -23,14 +22,15 @@ module.exports = {
         "no-inner-declarations": "error",
         "no-invalid-regexp": "error",
         "no-irregular-whitespace": "error",
-        "no-negated-in-lhs": "error",
         "no-obj-calls": "error",
+        "no-prototype-builtins": "error",
         "no-regex-spaces": "error",
         "no-sparse-arrays": "error",
         "no-template-curly-in-string": "error",
         "no-unexpected-multiline": "off",
         "no-unreachable": "error",
         "no-unsafe-finally": "error",
+        "no-unsafe-negation": "error",
         "use-isnan": "error",
         "valid-jsdoc": "off",
         "valid-typeof": ["error", { requireStringLiterals: true }]
