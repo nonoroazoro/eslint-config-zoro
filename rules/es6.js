7f8966fb2f318d789a77dbdcf5f9e442baa4d6df
nonoroazoro <xiaochao.k@gmail.com>
2016-12-07 17:52:38 +0800

    update to eslint v3.11.1

--- rules/es6.js
+++ rules/es6.js
@@ -21,9 +21,7 @@ module.exports = {
         "constructor-super": "error",
         "generator-star-spacing": "off",
         "no-class-assign": "error",
-        "no-confusing-arrow": ["error", {
-            "allowParens": true
-        }],
+        "no-confusing-arrow": ["error", { "allowParens": true }],
         "no-const-assign": "error",
         "no-dupe-class-members": "error",
         "no-duplicate-imports": "error",
@@ -32,14 +30,24 @@ module.exports = {
         "no-this-before-super": "error",
         "no-useless-computed-key": "error",
         "no-useless-constructor": "error",
+        "no-useless-rename": ["error", {
+            "ignoreDestructuring": false,
+            "ignoreImport": false,
+            "ignoreExport": false
+        }],
         "no-var": "error",
         "object-shorthand": "off",
+        "prefer-arrow-callback": ["error", {
+            "allowNamedFunctions": false,
+            "allowUnboundThis": true
+        }],
         "prefer-const": "error",
-        "prefer-reflect": "off",
+        "prefer-numeric-literals": "error",
         "prefer-rest-params": "error",
         "prefer-spread": "off",
         "prefer-template": "error",
         "require-yield": "off",
+        "rest-spread-spacing": ["error", "never"],
         "sort-imports": "off",
         "symbol-description": "error",
         "template-curly-spacing": "error",
