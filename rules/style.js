5d46d496c511b3d1d1f150dce239d2b40d01dfa6
nonoroazoro <xiaochao.k@gmail.com>
2016-12-07 17:46:19 +0800

    update to eslint v3.11.1

--- rules/style.js
+++ rules/style.js
@@ -8,12 +8,14 @@ module.exports = {
         "block-spacing": ["error", "always"],
         "brace-style": ["error", "allman", { "allowSingleLine": true }],
         "camelcase": "off",
+        "comma-dangle": "error",
         "comma-spacing": ["error", { "before": false, "after": true }],
         "comma-style": ["error", "last"],
         "computed-property-spacing": ["error", "never"],
         "consistent-this": "off",
         "eol-last": "error",
         "func-call-spacing": ["error", "never"],
+        "func-name-matching": "off",
         "func-names": "warn",
         "func-style": "off",
         "id-blacklist": "off",
@@ -31,18 +33,27 @@ module.exports = {
                 "case": { "after": true }
             }
         }],
+        "line-comment-position": "off",
         "linebreak-style": "off",
         "lines-around-comment": "off",
+        "lines-around-directive": ["error", {
+            before: "always",
+            after: "always"
+        }],
         "max-depth": "off",
         "max-len": "off",
+        "max-lines": "off",
         "max-nested-callbacks": "off",
         "max-params": "off",
-        "max-statements": "off",
         "max-statements-per-line": "error",
+        "max-statements": "off",
         "new-cap": ["error", {
             "newIsCap": true,
             "capIsNewExceptions": [
-                "Router"
+                "Router",
+                "Immutable.Map",
+                "Immutable.Set",
+                "Immutable.List"
             ]
         }],
         "new-parens": "error",
@@ -54,6 +65,16 @@ module.exports = {
         "no-continue": "off",
         "no-inline-comments": "off",
         "no-lonely-if": "off",
+        "no-mixed-operators": ["error", {
+            "groups": [
+                ["+", "-", "*", "/", "%", "**"],
+                ["&", "|", "^", "~", "<<", ">>", ">>>"],
+                ["==", "!=", "===", "!==", ">", ">=", "<", "<="],
+                ["&&", "||"],
+                ["in", "instanceof"]
+            ],
+            "allowSamePrecedence": false
+        }],
         "no-mixed-spaces-and-tabs": "error",
         "no-multiple-empty-lines": ["warn", { "max": 2, "maxEOF": 1 }],
         "no-negated-condition": "off",
@@ -66,24 +87,29 @@ module.exports = {
             "LabeledStatement",
             "WithStatement"
         ],
-        "no-spaced-func": "error",
         "no-tabs": "error",
         "no-ternary": "off",
         "no-trailing-spaces": "error",
         "no-underscore-dangle": "off",
         "no-unneeded-ternary": "error",
         "no-whitespace-before-property": "error",
+        "object-curly-newline": ["off", {
+            "ObjectExpression": { "minProperties": 0, "multiline": true },
+            "ObjectPattern": { "minProperties": 0, "multiline": true }
+        }],
         "object-curly-spacing": ["error", "always"],
-        "one-var": ["error", "never"],
+        "object-property-newline": ["error", { "allowMultiplePropertiesPerLine": true }],
         "one-var-declaration-per-line": ["error", "always"],
+        "one-var": ["error", "never"],
         "operator-assignment": "off",
         "operator-linebreak": "off",
         "padded-blocks": ["error", "never"],
         "quote-props": ["error", "as-needed", { "keywords": false, "unnecessary": false, "numbers": false }],
         "quotes": ["error", "double"],
         "require-jsdoc": "off",
-        "semi": ["error", "always"],
         "semi-spacing": ["error", { "before": false, "after": true }],
+        "semi": ["error", "always"],
+        "sort-keys": "off",
         "sort-vars": "off",
         "space-before-blocks": "error",
         "space-before-function-paren": ["error", { "anonymous": "always", "named": "never" }],
@@ -101,6 +127,7 @@ module.exports = {
                 balanced: false
             }
         }],
+        "unicode-bom": ["error", "never"],
         "wrap-regex": "off"
     }
 };
