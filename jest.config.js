b9c0721babdf20e7f4f4c5cb570c563054b8c581
nonoroazoro <xiaochao.k@gmail.com>
2019-04-27 00:25:15 +0800

    add jest config

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "rootDir": ".",
+    "coverageDirectory": "<rootDir>/@coverage",
+    "moduleFileExtensions": ["js", "ts", "tsx"],
+    "testRegex": "/tests/.+\\.(test|spec)\\.tsx?$",
+    "transform": { "^.+\\.tsx?$": "ts-jest" },
+    "verbose": true
+};
