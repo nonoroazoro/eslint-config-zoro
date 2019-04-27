68689c6d4c1555d09465cd93afc42abcdd6eb345
nonoroazoro <xiaochao.k@gmail.com>
2019-04-27 14:19:56 +0800

    add typescript rules

--- src/rules/typescript.ts
+++ src/rules/typescript.ts
@@ -6,23 +6,43 @@ export = {
         "@typescript-eslint"
     ],
     "rules": {
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/explicit-member-accessibility": "off",
-        "@typescript-eslint/interface-name-prefix": "error",
-        "@typescript-eslint/no-empty-interface": [
+        "@typescript-eslint/adjacent-overload-signatures": "error",
+        "@typescript-eslint/array-type": ["error", "array-simple"],
+        "@typescript-eslint/ban-types": [
             "error",
             {
-                "allowSingleExtends": true
+                "types": {
+                    "Object": "Use {} instead",
+                    "String": {
+                        "message": "Use string instead",
+                        "fixWith": "string"
+                    }
+                }
             }
         ],
+        "@typescript-eslint/camelcase": "off",
+        "@typescript-eslint/class-name-casing": "error",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-member-accessibility": "off",
+        "@typescript-eslint/indent": "off",
+        "@typescript-eslint/interface-name-prefix": "error",
+        "@typescript-eslint/member-delimiter-style": "error",
+        "@typescript-eslint/no-angle-bracket-type-assertion": "error",
+        "@typescript-eslint/no-array-constructor": "error",
+        "@typescript-eslint/no-empty-interface": ["error", { "allowSingleExtends": true }],
         "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-use-before-define": [
-            "error",
-            {
-                "functions": false,
-                "classes": true,
-                "variables": true
-            }
-        ]
+        "@typescript-eslint/no-inferrable-types": "off",
+        "@typescript-eslint/no-misused-new": "error",
+        "@typescript-eslint/no-namespace": ["error", { "allowDeclarations": true, "allowDefinitionFiles": true }],
+        "@typescript-eslint/no-non-null-assertion": "warn",
+        "@typescript-eslint/no-object-literal-type-assertion": "error",
+        "@typescript-eslint/no-parameter-properties": "error",
+        "@typescript-eslint/no-triple-slash-reference": "error",
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-use-before-define": "off",
+        "@typescript-eslint/no-var-requires": "error",
+        "@typescript-eslint/prefer-interface": "error",
+        "@typescript-eslint/prefer-namespace-keyword": "error",
+        "@typescript-eslint/type-annotation-spacing": "error"
     }
 };
