ed5a4570f77349e66bb7ec27bb2e7e033b053053
Nonoroazoro <xiaochao.k@gmail.com>
2019-11-22 17:48:02 +0800

    remove allowConstantLoopConditions

--- src/rules/typescript.ts
+++ src/rules/typescript.ts
@@ -89,7 +89,7 @@ export = {
         "@typescript-eslint/no-require-imports": "off",
         "@typescript-eslint/no-this-alias": ["error", { "allowedNames": ["self"] }],
         "@typescript-eslint/no-type-alias": "off",
-        "@typescript-eslint/no-unnecessary-condition": ["error", { "ignoreRhs": true, "allowConstantLoopConditions": true }],
+        "@typescript-eslint/no-unnecessary-condition": ["error", { "ignoreRhs": true }],
         "@typescript-eslint/no-unnecessary-qualifier": "error",
         "@typescript-eslint/no-unnecessary-type-arguments": "off",
         "@typescript-eslint/no-unnecessary-type-assertion": "error",
