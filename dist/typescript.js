c46ca38a496900f209c75b0f75e138df7e358c94
Nonoroazoro <xiaochao.k@gmail.com>
2019-12-04 11:51:32 +0800

    only lint ts files with ts rules

--- dist/typescript.js
+++ dist/typescript.js
@@ -1,12 +1,17 @@
 "use strict";
 module.exports = {
-    "extends": require.resolve("./rules/typescript"),
-    "env": {
-        "browser": true
-    },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    }
+    "overrides": [
+        {
+            "files": ["*.ts", "*.tsx"],
+            "extends": require.resolve("./rules/typescript"),
+            "env": {
+                "browser": true
+            },
+            "parser": "@typescript-eslint/parser",
+            "parserOptions": {
+                "ecmaVersion": 2018,
+                "sourceType": "module"
+            }
+        }
+    ]
 };
--- src/typescript.ts
+++ src/typescript.ts
@@ -2,13 +2,18 @@
  * Includes `TypeScript` rules.
  */
 export = {
-    "extends": require.resolve("./rules/typescript"),
-    "env": {
-        "browser": true
-    },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    }
+    "overrides": [
+        {
+            "files": ["*.ts", "*.tsx"],
+            "extends": require.resolve("./rules/typescript"),
+            "env": {
+                "browser": true
+            },
+            "parser": "@typescript-eslint/parser",
+            "parserOptions": {
+                "ecmaVersion": 2018,
+                "sourceType": "module"
+            }
+        }
+    ]
 };
