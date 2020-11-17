342714b55553233417799bdf94743846608e064a
Nonoroazoro <xiaochao.k@gmail.com>
2020-11-17 11:48:14 +0800

    add react rules

--- dist/rules/react.js
+++ dist/rules/react.js
@@ -118,6 +118,7 @@ module.exports = {
         "react/jsx-key": "off",
         "react/jsx-max-depth": "off",
         "react/jsx-max-props-per-line": "off",
+        "react/jsx-newline": "off",
         "react/jsx-no-bind": [
             "error",
             {
@@ -129,6 +130,7 @@ module.exports = {
             }
         ],
         "react/jsx-no-comment-textnodes": "error",
+        "react/jsx-no-constructed-context-values": "error",
         "react/jsx-no-duplicate-props": "error",
         "react/jsx-no-literals": "off",
         "react/jsx-no-script-url": "error",
