7d242a6af554bd3536432717d412025536494f2a
Nonoroazoro <xiaochao.k@gmail.com>
2023-10-11 17:45:26 +0800

    disable @typescript-eslint/no-unsafe-enum-comparison

--- src/rules/typescript.ts
+++ src/rules/typescript.ts
@@ -172,7 +172,7 @@ export = {
         "@typescript-eslint/no-unsafe-assignment": "off",
         "@typescript-eslint/no-unsafe-call": "off",
         "@typescript-eslint/no-unsafe-declaration-merging": "error",
-        "@typescript-eslint/no-unsafe-enum-comparison": "error",
+        "@typescript-eslint/no-unsafe-enum-comparison": "off",
         "@typescript-eslint/no-unsafe-member-access": "off",
         "@typescript-eslint/no-unsafe-return": "off",
         "@typescript-eslint/no-unused-expressions": [
