169b31d6e6004d3641559286a794858bdef8e855
Nonoroazoro <xiaochao.k@gmail.com>
2024-09-09 18:12:20 +0800

    fix eslint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,32 @@
-export default {
-    extends: [
-        "./dist/eslint.js",
-        "./dist/stylistic.js",
-        "./dist/typescript.js"
-    ],
-    env: {
-        "jest": true
+import * as globals from "globals";
+
+import { ESLINT_CONFIGS } from "./dist/eslint.js";
+import { STYLISTIC_CONFIGS } from "./dist/stylistic.js";
+import { TYPESCRIPT_CONFIGS } from "./dist/typescript.js";
+
+export default [
+    ...ESLINT_CONFIGS,
+    ...STYLISTIC_CONFIGS,
+    ...TYPESCRIPT_CONFIGS,
+    {
+        ignores: [
+            "dist/*",
+            "eslint.js",
+            "index.js",
+            "jest.config.js",
+            "react.js",
+            "stylistic.js",
+            "typescript.js"
+        ]
     },
-    parserOptions: {
-        "project": "./tsconfig.json"
-    },
-    ignores: [
-        "dist",
-        "eslint.js",
-        "index.js",
-        "jest.config.js",
-        "react.js",
-        "stylistic.js",
-        "typescript.js",
-    ]
-};
+    {
+        languageOptions: {
+            globals: {
+                ...globals.jest
+            },
+            parserOptions: {
+                project: "./tsconfig.eslint.json"
+            }
+        }
+    }
+];
