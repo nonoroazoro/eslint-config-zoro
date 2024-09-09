018a202d0ce9cad100680066ca41d64d470bee2a
Nonoroazoro <xiaochao.k@gmail.com>
2024-09-09 18:12:03 +0800

    export all configs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-/**
- * Includes `all` rules.
- */
-export default {
-    "extends": [
-        "./eslint",
-        "./node",
-        "./react",
-        "./stylistic",
-        "./typescript"
-    ].map(path => require.resolve(path))
-};
+import { ESLINT_CONFIGS } from "./eslint"
+// import { NODE_CONFIGS } from "./node";
+import { REACT_CONFIGS } from "./react";
+import { STYLISTIC_CONFIGS } from "./stylistic";
+import { TYPESCRIPT_CONFIGS } from "./typescript";
+
+export * from "./eslint";
+// export * from "./node";
+export * from "./react";
+export * from "./stylistic";
+export * from "./typescript";
+
+// TODO: Node is not supported yet, fix after the eslint-plugin-node is updated.
+export const ALL_CONFIGS = [
+    ...ESLINT_CONFIGS,
+    // ...NODE_CONFIGS,
+    ...REACT_CONFIGS,
+    ...STYLISTIC_CONFIGS,
+    ...TYPESCRIPT_CONFIGS,
+]
