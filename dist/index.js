28fce63f0f1166bbe26dded2ad8e236d5f0167d1
Nonoroazoro <xiaochao.k@gmail.com>
2024-09-09 19:02:13 +0800

    fix index

--- dist/index.js
+++ dist/index.js
@@ -1,35 +1,39 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ALL_CONFIGS = void 0;
+exports.TYPESCRIPT_CONFIGS = exports.STYLISTIC_CONFIGS = exports.REACT_CONFIGS = exports.ESLINT_CONFIGS = exports.ALL_CONFIGS = void 0;
 const eslint_1 = require("./eslint");
-// import { NODE_CONFIGS } from "./node";
+// import { NODE_CONFIGS as RAW_NODE_CONFIGS } from "./node";
 const react_1 = require("./react");
 const stylistic_1 = require("./stylistic");
 const typescript_1 = require("./typescript");
-__exportStar(require("./eslint"), exports);
-// export * from "./node";
-__exportStar(require("./react"), exports);
-__exportStar(require("./stylistic"), exports);
-__exportStar(require("./typescript"), exports);
 // TODO: Node is not supported yet, fix after the eslint-plugin-node is updated.
+/**
+ * All configs, including eslint, node, react, stylistic, and typescript.
+ */
 exports.ALL_CONFIGS = [
     ...eslint_1.ESLINT_CONFIGS,
-    // ...NODE_CONFIGS,
+    // ...RAW_NODE_CONFIGS,
     ...react_1.REACT_CONFIGS,
     ...stylistic_1.STYLISTIC_CONFIGS,
     ...typescript_1.TYPESCRIPT_CONFIGS,
 ];
+/**
+ * Eslint configs.
+ */
+exports.ESLINT_CONFIGS = eslint_1.ESLINT_CONFIGS;
+/**
+ * Node configs.
+ */
+// export const NODE_CONFIGS = RAW_NODE_CONFIGS;
+/**
+ * React configs.
+ */
+exports.REACT_CONFIGS = react_1.REACT_CONFIGS;
+/**
+ * Stylistic configs.
+ */
+exports.STYLISTIC_CONFIGS = stylistic_1.STYLISTIC_CONFIGS;
+/**
+ * Typescript configs.
+ */
+exports.TYPESCRIPT_CONFIGS = typescript_1.TYPESCRIPT_CONFIGS;
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,43 @@
-import { ESLINT_CONFIGS } from "./eslint"
-// import { NODE_CONFIGS } from "./node";
-import { REACT_CONFIGS } from "./react";
-import { STYLISTIC_CONFIGS } from "./stylistic";
-import { TYPESCRIPT_CONFIGS } from "./typescript";
-
-export * from "./eslint";
-// export * from "./node";
-export * from "./react";
-export * from "./stylistic";
-export * from "./typescript";
+import { ESLINT_CONFIGS as RAW_ESLINT_CONFIGS } from "./eslint";
+// import { NODE_CONFIGS as RAW_NODE_CONFIGS } from "./node";
+import { REACT_CONFIGS as RAW_REACT_CONFIGS } from "./react";
+import { STYLISTIC_CONFIGS as RAW_STYLISTIC_CONFIGS } from "./stylistic";
+import { TYPESCRIPT_CONFIGS as RAW_TYPESCRIPT_CONFIGS } from "./typescript";
 
 // TODO: Node is not supported yet, fix after the eslint-plugin-node is updated.
+
+/**
+ * All configs, including eslint, node, react, stylistic, and typescript.
+ */
 export const ALL_CONFIGS = [
-    ...ESLINT_CONFIGS,
-    // ...NODE_CONFIGS,
-    ...REACT_CONFIGS,
-    ...STYLISTIC_CONFIGS,
-    ...TYPESCRIPT_CONFIGS,
-]
+    ...RAW_ESLINT_CONFIGS,
+    // ...RAW_NODE_CONFIGS,
+    ...RAW_REACT_CONFIGS,
+    ...RAW_STYLISTIC_CONFIGS,
+    ...RAW_TYPESCRIPT_CONFIGS,
+];
+
+/**
+ * Eslint configs.
+ */
+export const ESLINT_CONFIGS = RAW_ESLINT_CONFIGS;
+
+/**
+ * Node configs.
+ */
+// export const NODE_CONFIGS = RAW_NODE_CONFIGS;
+
+/**
+ * React configs.
+ */
+export const REACT_CONFIGS = RAW_REACT_CONFIGS;
+
+/**
+ * Stylistic configs.
+ */
+export const STYLISTIC_CONFIGS = RAW_STYLISTIC_CONFIGS;
+
+/**
+ * Typescript configs.
+ */
+export const TYPESCRIPT_CONFIGS = RAW_TYPESCRIPT_CONFIGS;
