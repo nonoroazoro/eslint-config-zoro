789f00fdb1f5999a03250ccc93cc22dcf3027b06
Nonoroazoro <xiaochao.k@gmail.com>
2025-08-22 16:30:55 +0800

    enable node rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import globals from "globals";
 
 import * as eslint from "./dist/eslint.js";
+import * as node from "./dist/node.js";
 import * as stylistic from "./dist/stylistic.js";
 import * as typescript from "./dist/typescript.js";
 
 export default [
+    ...node.NODE_CONFIGS,
     ...eslint.ESLINT_CONFIGS,
     ...stylistic.STYLISTIC_CONFIGS,
     ...typescript.TYPESCRIPT_CONFIGS,
