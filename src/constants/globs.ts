0279d2844b75eaa6a8a4eb4f996d7984d737fbce
Nonoroazoro <xiaochao.k@gmail.com>
2026-01-21 16:03:58 +0800

    add globs

---
+++ src/constants/globs.ts
@@ -0,0 +1,12 @@
+/**
+ * From https://github.com/antfu/eslint-config/blob/main/src/globs.ts
+ */
+
+export const GLOB_ALL = "**/*.?([cm])[jt]s?(x)";
+
+export const GLOB_JS = "**/*.?([cm])js";
+export const GLOB_JSX = "**/*.?([cm])jsx";
+
+export const GLOB_TS = "**/*.?([cm])ts";
+export const GLOB_TSX = "**/*.?([cm])tsx";
+export const GLOB_DTS = "**/*.d.ts";
