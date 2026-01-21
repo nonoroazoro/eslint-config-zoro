6634694b9514bfdf7618ff0248ac8ed02def5d7e
Nonoroazoro <xiaochao.k@gmail.com>
2026-01-21 16:43:34 +0800

    migrate to defineConfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,18 @@
 import globals from "globals";
 
-import * as eslint from "./dist/eslint.js";
-import * as node from "./dist/node.js";
-import * as stylistic from "./dist/stylistic.js";
-import * as typescript from "./dist/typescript.js";
+import { defineConfig } from "./dist/index.js";
 
-export default [
-    ...node.NODE_CONFIGS,
-    ...eslint.ESLINT_CONFIGS,
-    ...stylistic.STYLISTIC_CONFIGS,
-    ...typescript.TYPESCRIPT_CONFIGS,
-    {
-        ignores: [
-            "dist/*",
-            "jest.config.js"
-        ]
-    },
-    {
-        languageOptions: {
-            globals: {
-                ...globals.jest
-            },
-            parserOptions: {
-                project: "./tsconfig.eslint.json"
-            }
+export default defineConfig({
+    node: true,
+    typescript: true,
+
+    ignores: ["dist/**"],
+    languageOptions: {
+        globals: {
+            ...globals.jest
+        },
+        parserOptions: {
+            project: "./tsconfig.eslint.json"
         }
     }
-];
+});
